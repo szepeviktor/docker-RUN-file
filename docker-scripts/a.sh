d395a2cd266bbcbac3810e02e5629ae33e2c5b05
Viktor Szépe <viktor@szepe.net>
2023-06-27 08:04:41 +0000

    fix

--- docker-scripts/a.sh
+++ docker-scripts/a.sh
@@ -3,9 +3,15 @@
 set -e
 
 echo 'I would like to cut some trees from your jungle.'
-
 echo '🌲🦋🐌🐛🐜🐝'
 
-echo -n '$ '; pwd
+cd /tmp/
 
-find /opt
+./install-php-phive
+phive install n98-magerun2 --trust-gpg-keys E92FF862BCBFE89A
+find .
+
+echo 'OK.'
+
+# Cleanup
+rm -rf /tmp/*
