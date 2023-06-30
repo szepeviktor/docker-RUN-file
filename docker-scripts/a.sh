617b080c6a3e76455adeee3e004628fca1e5c1c8
Viktor Szépe <viktor@szepe.net>
2023-06-30 16:38:57 +0000

    next

--- docker-scripts/a.sh
+++ docker-scripts/a.sh
@@ -28,7 +28,10 @@ find .
 
 # Cleanup
 rm -r /tmp/*
+echo pre-clean;du -m /var/lib/{apt,dpkg}/
 apt-get clean
+echo post-clean;du -m /var/lib/{apt,dpkg}/
 rm -r /var/lib/apt/lists/*
+echo post-rm;du -m /var/lib/{apt,dpkg}/
 
 echo 'OK.'
